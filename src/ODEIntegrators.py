17f85df21aa3b552e64197e217ad33c5a387860f
Astro-Hy <htruong8950@sdsu.edu>
2025-09-15 23:38:13 -0700

    Completed ODEIntegrators.py

--- src/ODEIntegrators.py
+++ src/ODEIntegrators.py
@@ -1,4 +1,6 @@
-"""ODE integrator class to compute N-body gravitational dynamics"""
+"""
+ODE integrator class to compute N-body gravitational dynamics
+"""
 
 import numpy as np
 
@@ -58,7 +60,7 @@ class ODEIntegrator:
 
     def step_euler(self, current_positions, current_velocities, dt):
         """
-        Advance one step from Euler's method
+        Advance one step using Euler's method
         
         Parameters
         ----------
@@ -80,5 +82,116 @@ class ODEIntegrator:
         new_positions  = current_positions  + dt * current_velocities
         new_velocities = current_velocities + dt * accelerations
 
+        # Return new positions and new velocities for each object
+        return new_positions, new_velocities
+    
+    def step_rk2(self, current_positions, current_velocities, dt):
+        """
+        Advance one step using 2nd-order Runge-Kutta (RK2) method
+
+        Parameters
+        ----------
+        current_positions  : (N, 3) array
+            Current positions (r_n) in AU
+        current_velocities : (N, 3) array
+            Current velocities (v_n) in AU/yr
+        dt                 : float
+            Time step (Δt) in years
+
+        Returns
+        -------
+        new_positions, new_velocities : (N, 3) arrays
+            New positions (r_n+1) and new velocities (v_n+1)
+        """
+
+        # Algorithmic structure for RK2 method
+        k1_r = current_velocities
+        k1_v = self.compute_accelerations(current_positions)
+
+        midpoint_positions  = current_positions  + 0.5 * dt * k1_r
+        midpoint_velocities = current_velocities + 0.5 * dt * k1_v
+
+        k2_r = midpoint_velocities
+        k2_v = self.compute_accelerations(midpoint_positions)
+
+        new_positions  = current_positions  + dt * k2_r
+        new_velocities = current_velocities + dt * k2_v
+
+        # Return new positions and new velocities for each object
+        return new_positions, new_velocities
+
+    def step_rk4(self, current_positions, current_velocities, dt):
+        """
+        Advance one step using 4th-order Runge-Kutta (RK4) method
+
+        Parameters
+        ----------
+        current_positions  : (N, 3) array
+            Current positions (r_n) in AU
+        current_velocities : (N, 3) array
+            Current velocities (v_n) in AU/yr
+        dt                 : float
+            Time step (Δt) in years
+
+        Returns
+        -------
+        new_positions, new_velocities : (N, 3) arrays
+            New positions (r_n+1) and new velocities (v_n+1)
+        """
+
+        # Algorithmic structure for RK4 method
+        k1_r = current_velocities
+        k1_v = self.compute_accelerations(current_positions)
+
+        pos2 = current_positions  + 0.5 * dt * k1_r
+        vel2 = current_velocities + 0.5 * dt * k1_v
+        k2_r = vel2
+        k2_v = self.compute_accelerations(pos2)
+
+        pos3 = current_positions  + 0.5 * dt * k2_r
+        vel3 = current_velocities + 0.5 * dt * k2_v
+        k3_r = vel3
+        k3_v = self.compute_accelerations(pos3)
+
+        pos4 = current_positions  + dt * k3_r
+        vel4 = current_velocities + dt * k3_v
+        k4_r = vel4
+        k4_v = self.compute_accelerations(pos4)
+
+        new_positions  = current_positions  + \
+            (dt/6) * (k1_r + 2*k2_r + 2*k3_r + k4_r)
+        new_velocities = current_velocities + \
+            (dt/6) * (k1_v + 2*k2_v + 2*k3_v + k4_v)
+
+        # Return new positions and new velocities for each object
+        return new_positions, new_velocities
+    
+    def step_leapfrog(self, current_positions, current_velocities, dt):
+        """
+        Advance one step using the Leapfrog method
+
+        Parameters
+        ----------
+        current_positions  : (N, 3) array
+            Current positions (r_n) in AU
+        current_velocities : (N, 3) array
+            Current velocities (v_n) in AU/yr
+        dt                 : float
+            Time step (Δt) in years
+
+        Returns
+        -------
+        new_positions, new_velocities : (N, 3) arrays
+            New positions (r_n+1) and new velocities (v_n+1)
+        """
+
+        # Algorithmic structure for Leapfrog method
+        current_accelerations = self.compute_accelerations(current_positions)
+        halfway_velocities = current_velocities + 0.5 * dt * current_accelerations
+
+        new_positions     = current_positions + dt * halfway_velocities
+        new_accelerations = self.compute_accelerations(new_positions)
+        new_velocities    = halfway_velocities + 0.5 * dt * new_accelerations
+        
         # Return new positions and new velocities for each object
         return new_positions, new_velocities
